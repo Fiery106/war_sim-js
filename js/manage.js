fab1c72a2e02c73553210c40d75c5f45797bd6ad
Fiery106 <fiery106@gmail.com>
2025-07-01 13:48:27 +0200

    Map implementation 1
    1. Changed some of the classes into ids instead
    2. You can now dynamically create a new map when starting the game, however only the names will be randomly generated (FOR NOW)
    3. The names were added thanks to ChatGPT
    4. Updated some css variables to make things look nicer
    5. Organised some things here and there

--- js/manage.js
+++ js/manage.js
@@ -1,10 +1,14 @@
 let battle_screen = document.getElementById("battle-screen");
+let map_columns = 5;
+let map_rows = 5;
 
 let content_names = [
     "Manage your squad", 
     "Organize your items", 
     "Develop research"
 ]
+
+showMap()
 
 /*
 ready()
@@ -22,27 +26,63 @@ function genMap() {
 
     showMap()
 }
-
-
-            <div id="content-header">
-                <h1 id="content-head"> <!-- Manage your squad -->
-                    hello
-                </h1>
-                <p id="content-description">
-                    Lorem ipsum dolor sit amet consectetur.
-                </p>
-
-            </div>
 */
 
-function showMap() {
-    battle_screen.innerHTML = ""
+async function showMap() {
+    clearScreen()
+
+    let div = document.createElement("div"); //#map
+    
+    div.setAttribute("id", "map");
+
+    for (let i = 0; i < map_columns; i++) {
+        for (let j = 0; j < map_rows; j++) {
+            let button = document.createElement("button") //.land-tile
+            let name = document.createElement("h1"); //.land-name
+            let type = document.createElement("h2"); //.land-type
+            let info = document.createElement("div"); //.land-info
+            let power = document.createElement("h3"); //.land-power
+            let units = document.createElement("div"); //.land-units
+            let unit1 = document.createElement("img"); //.unit
+            let unit2 = document.createElement("img"); //.unit
+            let res = document.createElement("img"); //.land-resource
+            let src = await fecthJson()
+
+            button.classList.add("land-tile");
+            name.classList.add("land-name");
+            type.classList.add("land-type");
+            info.classList.add("land-info");
+            power.classList.add("land-power");
+            units.classList.add("land-units");
+            unit1.classList.add("unit");
+            unit2.classList.add("unit");
+            res.classList.add("land-resource");
+
+            name.textContent = src[Math.floor(Math.random() * src.length)];
+            type.textContent = "Type";
+            power.textContent = "99";
+            unit1.src = "images/hhh-funny-cat-face-v0-aic7sbhrv8pb1.webp"
+            unit2.src = "images/hhh-funny-cat-face-v0-aic7sbhrv8pb1.webp"
+            res.src = "images/hhh-funny-cat-face-v0-aic7sbhrv8pb1.webp"
+            /*
+            button.onclick = function() {
+                ?????????????
+            };
+            */
+            units.append(unit1, unit2) //show units on the tile
+            info.append(power, units, res) //
+            button.append(name, type, info)
+            div.append(button) //add the land tile to the map
+        }
+    }
+
+    battle_screen.append(div)
 }
 
 
 
 function openManageScreen(x) {
-    clearScreen() //if we don't clear the screen, we just keep adding more menus stacked on top of each other
+    clearScreen() //if we don't clear the screen, it will keep adding more menus stacked on top of each other
 
     let div = document.createElement("div");
     let h1 = document.createElement("h1");
@@ -55,11 +95,11 @@ function openManageScreen(x) {
     let hire_button = document.createElement("button");
     
 
-    div.classList.add("content-header");
-    h1.classList.add("content-head");
-    content.classList.add("content"); //flex
+    div.setAttribute("id", "content-header");
+    h1.setAttribute("id", "content-head");
+    content.setAttribute("id", "content"); //flex
 
-    flex.classList.add("button-field");
+    flex.setAttribute("id", "button-field");
     back_button.classList.add("back-button");
     hire_button.classList.add("back-button"); //this might seem confusing, but we're just copy-pasting the border styling
     back.classList.add("button-text");
@@ -67,6 +107,7 @@ function openManageScreen(x) {
 
     back.textContent = "Back";
     hire.textContent = "Buy";
+    
     //hire_button.onclick = ???
     back_button.onclick = function() {
         showMap()
@@ -121,3 +162,10 @@ function viewOptions(x, y, z) {
 function clearScreen() {
     battle_screen.innerHTML = ""
 }
+
+async function fecthJson() {
+        let url = "json/land_names.json"
+        let res = await fetch(url)
+        let data = await res.json()
+        return data.names
+    }
